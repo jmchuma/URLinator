ce6866a9c12552aaaab86475dcb3c0b0b0df745f
Jose M. Chumo Mata <jmchuma@gmail.com>
2012-02-21 07:54:58 +0100

    Adding internationalization
    I've added internationalization. I'm only supporting English and Spanish
    but I'll be adding Frencho soon. I'm showing a popup when the buttons are
    pressed since the clipboard think is trickier than a thought.
    
    TODO:
    - Internationalice the textarea placeholder

--- script.js
+++ script.js
@@ -1,17 +1,41 @@
-﻿
-
-function URLsToPasteBoard() {
-	var URLs = new Array();
-	
-	for (var i = 0; i < tabIds.length; i++) {
-      chrome.tabs.update(tabIds[i], updateTabData(tabIds[i]));
-	  var url = chrome.tabs.get(tabIds[i], getURL);
-	  URLs.push(url);
-    }
-	
-	
+/**
+ * Use for not implemented things
+ */
+function toDo() {
+	alert("Sorry, this is not yet implemented");
 }
 
-function getURL(Tab tab) {
+/**
+ * Initialize things in the HTML popup:
+ *	- Translate interface
+ */
+function init() {
+	var objects = document.getElementsByTagName('*');
+
+	for(var i = 0; i < objects.length; i++) {
+		// to understand this see HTML5 custom attributes
+		if(objects[i].dataset && objects[i].dataset.i18nId) {
+			objects[i].innerHTML = chrome.i18n.getMessage(objects[i].dataset.i18nId);
+		}
+	}
+}
+
+/**
+ *
+ */
+function URLsToPasteBoard() {
+	var URLs = new Array();
+
+	for(var i = 0; i < tabIds.length; i++) {
+		chrome.tabs.update(tabIds[i], updateTabData(tabIds[i]));
+		var url = chrome.tabs.get(tabIds[i], getURL);
+		URLs.push(url);
+	}
+}
+
+/**
+ *
+ */
+function getURL(tab) {
 	return tab.url;
 }
