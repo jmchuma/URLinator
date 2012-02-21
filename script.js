014d7f30af499cd558bae6b9685e254f8a73841c
Jose M. Chumo Mata <jmchuma@gmail.com>
2012-02-21 01:55:54 +0100

    Initial commit
    This is the first commit. I not realy sure of what I'm doing.
    For the time being the icon will be a white square. I'm working
    on ideas for it.

---
+++ script.js
@@ -0,0 +1,17 @@
+﻿
+
+function URLsToPasteBoard() {
+	var URLs = new Array();
+	
+	for (var i = 0; i < tabIds.length; i++) {
+      chrome.tabs.update(tabIds[i], updateTabData(tabIds[i]));
+	  var url = chrome.tabs.get(tabIds[i], getURL);
+	  URLs.push(url);
+    }
+	
+	
+}
+
+function getURL(Tab tab) {
+	return tab.url;
+}
