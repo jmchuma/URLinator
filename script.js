9375b78424aa4f69f4f9bc261d992d535e12b953
Jose M. Chumo Mata <jmchuma@gmail.com>
2012-02-24 01:13:38 +0100

    Just pushing
    Nothing interesting on this one. Must learn how to deb with Chrome.
    
    * toDo fuction removed
    
    * tabsToBoard fuction declared. Will copy from tabs to pasteboard
    
    * boardToTabs fuction declared. Will copy from board to tabs URLs

--- script.js
+++ script.js
@@ -1,11 +1,4 @@
 /**
- * Use for not implemented things
- */
-function toDo() {
-	alert("Sorry, this is not yet implemented");
-}
-
-/**
  * Initialize things in the HTML popup:
  *	- Translate interface
  */
@@ -20,10 +13,12 @@ function init() {
 	}
 }
 
+
 /**
  *
  */
 function URLsToPasteBoard() {
+	/*
 	var URLs = new Array();
 
 	for(var i = 0; i < tabIds.length; i++) {
@@ -31,6 +26,7 @@ function URLsToPasteBoard() {
 		var url = chrome.tabs.get(tabIds[i], getURL);
 		URLs.push(url);
 	}
+	*/
 }
 
 /**
@@ -39,3 +35,52 @@ function URLsToPasteBoard() {
 function getURL(tab) {
 	return tab.url;
 }
+
+/**
+ *
+ */
+function countWindowTabs(win) {
+	return win.tabs.length;
+}
+
+/**
+ * Copies from clipboard to tabs
+ */
+function boardToTabs() {
+	alert('boardToTabs is not ready yet');
+}
+
+/**
+ * Copies from tabs to clipboard
+ */
+function tabsToBoard() {
+	//alert('tabsToBoard is not ready yet');
+	
+	var box = document.getElementById('urls');
+	box.innerHTML = 'tabsToBoard is not ready yet';
+	//var urls = new Array();
+
+	/*
+	for(var i = 0; i < tabIds.length; i++) {
+		chrome.tabs.update(tabIds[i], updateTabData(tabIds[i]));
+		var url = chrome.tabs.get(tabIds[i], getURL);
+		urls.push(url);
+	}
+	*/
+
+	//var numTabs = chrome.windows.getCurrent(countWindowTabs);
+
+	/*
+	for(var i = 0; i < 3; i++) {
+		chrome.tabs.update(tabIds[i], updateTabData(tabIds[i]));
+		var url = chrome.tabs.get(tabIds[i], getURL);
+		urls.push(url);
+	}
+
+	alert(urls.join('\n'));
+	*/
+
+	//var textbox = document.getElementById('urls');
+	//textbox.innerHTML =  urls.join('\n');
+}
+
