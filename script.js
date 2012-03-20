a9a7da5296f117b2c5ac79c8a4c637f57e7c51a5
Jose M. Chumo Mata <jmchuma@gmail.com>
2012-03-20 20:39:25 +0100

    Copy all URLs to textbox
    Copy URLs from all tabs of all open windows to the textbox.
    
    TODO: - fix this to copy only from tabs of the current window
          - add a checkbox to toggle between all and current?

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+var url_list = new Array();
+
+
 /**
  * Initialize things in the HTML popup:
  *	- Translate interface
@@ -15,72 +18,34 @@ function init() {
 
 
 /**
- *
- */
-function URLsToPasteBoard() {
-	/*
-	var URLs = new Array();
-
-	for(var i = 0; i < tabIds.length; i++) {
-		chrome.tabs.update(tabIds[i], updateTabData(tabIds[i]));
-		var url = chrome.tabs.get(tabIds[i], getURL);
-		URLs.push(url);
-	}
-	*/
-}
-
-/**
- *
- */
-function getURL(tab) {
-	return tab.url;
-}
-
-/**
- *
- */
-function countWindowTabs(win) {
-	return win.tabs.length;
-}
-
-/**
- * Copies from clipboard to tabs
+ * Copies URL from clipboard to tabs
  */
 function boardToTabs() {
 	alert('boardToTabs is not ready yet');
 }
 
+
 /**
- * Copies from tabs to clipboard
+ * Copies URL from tabs to clipboard
  */
 function tabsToBoard() {
-	//alert('tabsToBoard is not ready yet');
-	
 	var box = document.getElementById('urls');
-	box.innerHTML = 'tabsToBoard is not ready yet';
-	//var urls = new Array();
 
 	/*
-	for(var i = 0; i < tabIds.length; i++) {
-		chrome.tabs.update(tabIds[i], updateTabData(tabIds[i]));
-		var url = chrome.tabs.get(tabIds[i], getURL);
-		urls.push(url);
-	}
-	*/
+	chrome.windows.getLastFocused(function(win){
+		for(tab in win.tabs) {
+			url_list.push(tab.url);
+		}
+	});
+	/**/
+	chrome.windows.getAll({populate: true}, function(wins_array){
+		wins_array.forEach(function(win) {
+			win.tabs.forEach(function(tab) {
+				url_list.push(tab.url);
+			});
+		});
+	});
 
-	//var numTabs = chrome.windows.getCurrent(countWindowTabs);
-
-	/*
-	for(var i = 0; i < 3; i++) {
-		chrome.tabs.update(tabIds[i], updateTabData(tabIds[i]));
-		var url = chrome.tabs.get(tabIds[i], getURL);
-		urls.push(url);
-	}
-
-	alert(urls.join('\n'));
-	*/
-
-	//var textbox = document.getElementById('urls');
-	//textbox.innerHTML =  urls.join('\n');
+	box.innerHTML = url_list.join('\n');
 }
 
